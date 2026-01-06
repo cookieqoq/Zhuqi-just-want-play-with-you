8bcd6780c19c1291887f4bdd04ecb91e3320eccb
cookieqoq <s1134531@g.ncyu.edu.tw>
2026-01-07 00:04:17 +0800

    text and comments

--- obj/Debug/net10.0/網頁期末專題.AssemblyInfo.cs
+++ obj/Debug/net10.0/網頁期末專題.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("網頁期末專題")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ce796d4204711dd18a3fa921d077c9ea10322c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4739629b9cb4221065965c99718b16d648d7456")]
 [assembly: System.Reflection.AssemblyProductAttribute("網頁期末專題")]
 [assembly: System.Reflection.AssemblyTitleAttribute("網頁期末專題")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
